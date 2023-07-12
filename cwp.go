8553bf573ba2e8db437b71a66deba1783b9d43f7
NakaokaTomoki <75064971+NakaokaTomoki@users.noreply.github.com>
2023-07-13 01:59:37 +0900

    modify

--- cwp.go
+++ cwp.go
@@ -2,22 +2,34 @@ package cwp
 
 import "fmt"
 
-type Weather struct {
- 	Place        string
+type OpenWeather struct {
+	Place              string    `json:"name"`
+	Weather            []Weather `json:"weather"`
+        Temp               map[string]interface{}   `json:"main"`
+	DateTime           int64     `json:"dt"`
 	// Shorten   string `json:"link"`
 	// Original  string `json:"long_url"`
 	// IsDeleted bool   `json:"is_deleted"`
 	// Group     string
 }
 
-func (weather *Weather) String() string {
-	return fmt.Sprintf("%s", weather.Place)
+type Weather struct {
+	Weather            string    `json:"main"`
+	WeatherDescription string    `json:"description"`
+}
+
+type Main struct {
+	Main            float64    `json:"main"`
+}
+
+func (openweather *OpenWeather) String() string {
+	return fmt.Sprintf("%s", openweather.Place)
 }
 
 type CurrentWeatherPrinter interface {
 	// List(config *Config) ([]*Weather, error)
 	// Shorten(config *Config, url string) (*Weather, error)
-	Weather(config *Config, place string) (*Weather, error)
+	OpenWeather(config *Config, place string) (*OpenWeather, error)
 	// Delete(config *Config, Weather string) error
 	// QRCode(config *Config, Weather string) ([]byte, error)
 }
